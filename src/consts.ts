37cff5ba02743224b9386225061de68990e05cf5
Franco Castany <francocastany@gmail.com>
2024-01-17 11:33:22 -0300

    small changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Futbol Actualizado"
+export const SITE_DESCRIPTION = "Información de futbol."
